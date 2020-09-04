44946a672d2537884d44c6bbef2f8c9519761991
xinyunaha <xinyunlk@outlook.com>
2020-09-05 00:45:38 +0800

    add: base player control

--- app/src/main/java/github/xinyunaha/music_hd/MainActivity.kt
+++ app/src/main/java/github/xinyunaha/music_hd/MainActivity.kt
@@ -6,29 +6,29 @@ package github.xinyunaha.music_hd
 import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.util.Log.d
 import android.util.Log.e
-import android.view.View
 import android.view.WindowManager
-import android.widget.Toast
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.source.ConcatenatingMediaSource
-import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.*
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import github.xinyunaha.music_hd.api.ApiEngine.Companion.retrofitCreate
 import github.xinyunaha.music_hd.api.ApiService
 import github.xinyunaha.music_hd.bean.checkMusic.checkMusic
 import github.xinyunaha.music_hd.fragment.*
+import github.xinyunaha.music_hd.util.PlayHandler
 import github.xinyunaha.music_hd.util.ToastUtil
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_find.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.ref.WeakReference
 
 
 open class MainActivity : AppCompatActivity() {
@@ -44,10 +44,41 @@ open class MainActivity : AppCompatActivity() {
         MusicName_text.isSelected = true
         MusicSinger_text.isSelected = true
 
-        stop_play_btn.setOnClickListener {
-            ToastUtil.toast(this,"fuck")
-            d("播放控制","暂停")
-        }
+
+        val url = "http://123.147.165.27:8899/m8.music.126.net/20200905003808/400a070b0855dcff4bac405b7cb61692/ymusic/76e5/ba34/d562/2e95d6640354faee9ef0d6a384d2bc5f.mp3"
+        val player = ExoPlayerFactory.newSimpleInstance(this@MainActivity, DefaultTrackSelector())
+        val defaultDataSourceFactory = DefaultDataSourceFactory(this@MainActivity, "audio/mpeg")
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        val mediaSource1 = ExtractorMediaSource.Factory(defaultDataSourceFactory)
+            .createMediaSource(Uri.parse(url))
+        concatenatingMediaSource.addMediaSource(mediaSource1)
+        d("播放列表数量","${concatenatingMediaSource.size}")
+        player.playWhenReady = true
+
+        val playHandlers = PlayHandler(seekBar, player)
+        concatenatingMediaSource.addEventListener(playHandlers, object : DefaultMediaSourceEventListener() {
+            override fun onLoadStarted(windowIndex: Int, mediaPeriodId: MediaSource.MediaPeriodId?, loadEventInfo: MediaSourceEventListener.LoadEventInfo?, mediaLoadData: MediaSourceEventListener.MediaLoadData?) {
+                super.onLoadStarted(windowIndex, mediaPeriodId, loadEventInfo, mediaLoadData)
+                seekBar.max = player.duration.toInt()
+                playHandlers.sendEmptyMessage(0)
+            }
+        })
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                player.seekTo(seekBar?.progress!!.toLong())
+            }
+
+        })
+        seekBar.onProg
+        // 进度条Handler
+        val playHandler = PlayHandler(seekBar, player)
 
         // 发请求的玩意
         val request = retrofitCreate(this).create(ApiService::class.java)
@@ -61,25 +92,28 @@ open class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<checkMusic>,response: Response<checkMusic>) {
                 d("网络访问","成功")
                 d("歌曲可用性","${response.body()?.success}")
-
-//                val url = "http://m8.music.126.net/20200903104437/668eb35edb81ee99a4fd23545f3f8ca3/ymusic/0fd6/4f65/43ed/a8772889f38dfcb91c04da915b301617.mp3"
-//                val player = ExoPlayerFactory.newSimpleInstance(this@MainActivity, DefaultTrackSelector())
-//                val defaultDataSourceFactory = DefaultDataSourceFactory(this@MainActivity, "audio/mpeg")
-//                val concatenatingMediaSource = ConcatenatingMediaSource()
-//                val mediaSource1 = ExtractorMediaSource.Factory(defaultDataSourceFactory)
-//                    .createMediaSource(Uri.parse(url))
-//                concatenatingMediaSource.addMediaSource(mediaSource1)
-//                d("播放列表数量","${concatenatingMediaSource.size}")
-//                player.playWhenReady = true
-//                player.prepare(concatenatingMediaSource)
+                player.prepare(concatenatingMediaSource)
             }
         })
+
+        // 开局跳转发现界面
+        supportFragmentManager.beginTransaction().replace(R.id.fragment,FindFragment()).addToBackStack(null).commit()
+
 //        find_btn.setOnClickListener {
 //            supportFragmentManager.beginTransaction().replace(R.id.fragment,FindFragment()).addToBackStack(null).commit()
 //        }
 
+        //停止播放监听
+        stop_play_btn.setOnClickListener {
+            if(player.playWhenReady){
+                d("播放控制","暂停")
+                player.playWhenReady = false
+            }else{
+                d("播放控制","播放")
+                player.playWhenReady = true
+            }
 
 
+        }
     }
-
 }
---
+++ app/src/main/java/github/xinyunaha/music_hd/util/PlayHandler.kt
@@ -0,0 +1,23 @@
+package github.xinyunaha.music_hd.util
+
+import android.os.Handler
+import android.os.Message
+import android.widget.SeekBar
+import com.google.android.exoplayer2.ExoPlayer
+import java.lang.ref.WeakReference
+
+class PlayHandler(seekBar: SeekBar, player: ExoPlayer): Handler(){
+    private val seekBar = WeakReference<SeekBar>(seekBar)
+    private val players = WeakReference<ExoPlayer>(player)
+
+    override fun handleMessage(msg: Message) {
+        super.handleMessage(msg)
+        when (msg.what){
+            0 ->{
+                seekBar.get()?.max = players.get()?.duration!!.toInt()
+                seekBar.get()?.progress = players.get()!!.currentPosition.toInt()
+                sendEmptyMessageDelayed(0,300)
+            }
+        }
+    }
+}
